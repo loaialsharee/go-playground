5289e1e495ca034475f72cad04f3df4f94f8097e
Loai Al-Sharee <loaialsharee1@gmail.com>
2024-01-14 01:49:31 +0800

    Struct Project Bite

---
+++ structs-project/go.mod
@@ -0,0 +1,3 @@
+module examples.com/note
+
+go 1.21.5
---
+++ structs-project/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"examples.com/note/note"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	note, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	note.Display()
+	err = note.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed.")
+		return
+	}
+
+	fmt.Println("Saving the note succeeded!")
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+
+	content := getUserInput("Note content:")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Printf("%v ", prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
