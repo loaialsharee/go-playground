d1215fcab081e7c45e0bb13dfd1e95dde4da4a0a
Loai Al-Sharee <loaialsharee1@gmail.com>
2024-01-17 21:29:58 +0800

    Experimenting for loops in maps and slices

--- arrays/make.go
+++ arrays/make.go
@@ -27,4 +27,14 @@ func main() {
 	courseRatings.output()
 
 	//fmt.Println(courseRatings)
+
+	for index, value := range userNames {
+		fmt.Println("Index: ", index)
+		fmt.Println("Value: ", value)
+	}
+
+	for key, value := range courseRatings {
+		fmt.Println("Key: ", key)
+		fmt.Println("Value: ", value)
+	}
 }
